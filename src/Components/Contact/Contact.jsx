8bdcac8b541e57ae9164e10308800b559f51d0a4
Aman Patel <patelpatelaman00@gmail.com>
2024-01-20 16:36:52 +0530

    email sending

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
+import emailjs from '@emailjs/browser';
+import { useRef } from "react";
 export const Contact = () => {
+
+    const form = useRef();
+
+    const [done,setDone] = useState(false)
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_uarzgce', 'template_hn2ovcd', form.current, 'pTpVmEEwwXDe3lyyh')
+        .then((result) => {
+            console.log(result.text);
+            setDone(true);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+  
+
   return (
     <div className="contact-form">
       {/* Left side of contact section */}
@@ -16,12 +35,12 @@ export const Contact = () => {
       </div>
       {/* Right Side of contact Section */}
       <div className="c-right">
-        <form >
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="user_name" className="user"  placeholder="Name"/>
           <input type="email" name="user_email" className="user" placeholder="Email"/>
           <textarea name="message" className="user" placeholder="Message"/>
           <input type="submit" value="Send" className="button"/>
-          <span>{  "Thanks for Contacting me"}</span>
+          <span>{  done &&  "Thanks for Contacting me"}</span>
           <div
             className="blur c-blur1"
             style={{ background: "var(--purple)" }}
